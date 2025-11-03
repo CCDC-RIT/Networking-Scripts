911b2d2cf682ac5045e755c4b6988de80f368bc8
Tri Tran <chemandtech874@gmail.com>
2025-11-03 17:46:40 -0500

    redefine backup dir

--- pfSense/core/backup.sh
+++ pfSense/core/backup.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 # Assuming that inventory is run first
-BACKUP_DIR="/usr/share/vt/fonts/blueteam"
+BACKUP_DIR=""
 GUI_SRC="/usr/local/www"
 CONFIG_SRC="/conf/config.xml"
 RULES_SRC="/tmp/rules.debug"
 AUTH_SRC="/etc/inc/auth.inc"
 
 backup() {
+    stty -echo
+    read -r "Backup directory: " BACKUP_DIR
+    stty echo
+    echo ""
+
     cp -r "$GUI_SRC" "$BACKUP_DIR/www"          # GUI PHP files
     cp "$CONFIG_SRC" "$BACKUP_DIR/config.xml"   # Main config file
     cp "$RULES_SRC" "$BACKUP_DIR/rules.debug"   # Debug rules file
