fcfe3913ab3038a3d36b20da65a2624f35076c57
Tri Tran <chemandtech874@gmail.com>
2025-11-03 18:09:22 -0500

    begin users with /etc/passwd

--- pfSense/core/users.sh
+++ pfSense/core/users.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-bake() {
-    echo "TODO"
+USERS_REFERENCE="../util/info/users.txt"
+ADMINS_REFERENCE="..util/info/admins.txt"
+
+users() {
+    while IFS=":" read -r user _ _ _ _ _ shell; do
+        if ! grep -qF -x "$user" "/etc/passwd" && ! $shell -ne "/usr/sbin/nologin"; then
+            echo "$user not in approved list!"
+        fi
+    done < "/etc/passwd"
 }
 
-bake
+users
