84612ba37147d66be0203d6774fe4da842da8a93
Tri Tran <chemandtech874@gmail.com>
2026-01-13 19:08:32 -0500

    pfSense stager

---
+++ downloads.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "Missing argument!"
+    exit
+fi
+
+download() {
+    if [ "$1" -eq "pfsense" ]; then
+        curl -L -O https://github.com/CCDC-RIT/Networking-Scripts/archive/refs/heads/main.zip
+        unzip main.zip
+        cd Networking-Scripts-main/pfSense
+        chmod +x start.sh
+    elif [ "$1" -eq "palo" ]; then
+        echo "TODO"
+    elif [ "$1" -eq "cisco" ]; then
+        echo "TODO"
+    else
+        echo "Invalid argument!"
+        exit
+    fi
+}
+
+downloads() {
+    download
+    echo "On your command captain!"
+}
+
+downloads
