1e93d50623b86e3d1d5dc860270b14f59246b3d0
naohvs <noahvs51906@gmail.com>
2026-01-19 10:54:11 -0500

    Added user input for file perms in secure

--- pfSense/core/secure.sh
+++ pfSense/core/secure.sh
@@ -7,7 +7,16 @@ file_perms() {
 }
 
 secure() {
-    file_perms
+    echo "Do you want to execute file_perms? (y/n)"
+    read answer
+    if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "YES" ]; then
+        file_perms
+        echo "Executing file_perms."
+    elif [ "$answer" = "n" ] || [ "$answer" = "N" ] || [ "$answer" = "no" ] || [ "$answer" = "NO" ]; then
+        echo "file_perms has not been executed"
+    else
+        echo "Invalid input. Please enter 'y' or 'n'."
+    fi
 }
 
 secure
