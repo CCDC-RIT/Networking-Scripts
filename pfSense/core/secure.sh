ad7df28bfcf49515f334bfe724b9ccc834bbc0af
naohvs <noahvs51906@gmail.com>
2026-01-19 11:28:58 -0500

    Added functionality to remove suspicious loaded modules based on user input

--- pfSense/core/secure.sh
+++ pfSense/core/secure.sh
@@ -6,16 +6,38 @@ file_perms() {
     chflags schg /etc/inc/auth.inc
 }
 
+remove_suspicious_modules() {
+    suspicious_modules=$(tr '\n' ' ' < ../util/info/suspicious_modules.txt)
+    for module in $suspicious_modules; do 
+        read -p "Do you want to unload the module $module? (y/n)" answer
+        answer= ${answer,,}
+        if [ "$answer" = "y" ] || [ "$answer" = "yes" ]; then
+            kldunload "$module" 
+            echo "Unloaded module: $module"
+        else
+            echo "Module $module has not been unloaded."
+        fi
+    done
+}
+
 secure() {
-    echo "Do you want to execute file_perms? (y/n)"
-    read answer
-    if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ] || [ "$answer" = "YES" ]; then
+    read -p "Do you want to execute file_perms? (y/n)" answer
+    answer= ${answer,,}
+    if [ "$answer" = "y" ] || [ "$answer" = "yes" ]; then
         file_perms
         echo "Executing file_perms."
-    elif [ "$answer" = "n" ] || [ "$answer" = "N" ] || [ "$answer" = "no" ] || [ "$answer" = "NO" ]; then
+    elif [ "$answer" = "n" ] || [ "$answer" = "no" ]; then
         echo "file_perms has not been executed"
     else
         echo "Invalid input. Please enter 'y' or 'n'."
+    fi
+
+    read -p "Do you want to unload suspicious kernel modules? (y/n)" answer
+    answer= ${answer,,}
+    if [ "$answer" = "y" ] || [ "$answer" = "yes" ]; then
+        remove_suspicious_modules
+    else
+        echo "Suspicious modules have not been unloaded."
     fi
 }
 
