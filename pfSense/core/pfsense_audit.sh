345f62283e69ce9b38b30fb42ea4db6722da1425
Tri Tran <chemandtech874@gmail.com>
2026-03-05 13:48:24 -0500

    support nested www

--- pfSense/core/pfsense_audit.sh
+++ pfSense/core/pfsense_audit.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 DEFAULT_WWW=$(cat ../util/info/default_www.txt)
+DEFAULT_NESTED_WWW=$(cat ../util/info/default_nested_www.txt)
 
 www() {
     echo "----- WWW -----"
     if [ -d /usr/local/www ]; then
         # shellcheck disable=SC2164
         cd /usr/local/www
+
         for file in *; do
             [ -e "$file" ] || break
 
@@ -21,8 +23,30 @@ www() {
     echo ""
 }
 
+nested_www() {
+    echo "----- NESTED WWW -----"
+    if [ -d /usr/local/www ]; then
+        # shellcheck disable=SC2164
+        cd /usr/local/www
+
+        set -- **/*
+        for file in "$@"; do
+            [ -e "$file" ] || break
+
+            if ! echo "$DEFAULT_NESTED_WWW" | grep -q "$file"; then
+                echo "Non-default nested GUI file: $file"
+            fi
+        done
+    fi
+
+    # shellcheck disable=SC2164
+    cd /home/root/pfSense/
+    echo ""
+}
+
 pfsense_audit() {
     www
+    nested_www
 }
 
 pfsense_audit
