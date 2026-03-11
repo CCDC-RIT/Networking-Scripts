510017f0fb4a26bb1840c06675eac8b99eee315f
naohvs <noahvs51906@gmail.com>
2026-03-11 00:57:23 -0400

    Changed Name, Added a Timer i think, add a log file for any other command besides wall or echo

---
+++ pfSense/util/files/sharp_cheddar.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+trap '' INT QUIT TERM HUP
+
+LOG_FILE="/var/log/ricotta.log"
+INTERVAL=600 #10 minutes
+LAST_EXECUTED=0
+
+while true
+do
+    printf ">"
+    read -r input
+
+    now=$(date +%s)
+    elapsed=$((now - LAST_EXECUTED))
+
+    case "$input" in
+        echo*)
+            if ["$elpased" -lt "$INTERVAL"]; then
+                remaining=$((INTERVAL - elapsed))
+                echo "Sorry, please wait $remaining seconds before using wall or echo again. Thanks!"
+            else
+                /bin/echo "${input#echo }"
+                LAST_EXECUTED=$now
+            fi
+            ;;
+        wall*)
+             if ["$elpased" -lt "$INTERVAL"]; then
+                remaining=$((INTERVAL - elapsed))
+                echo "Sorry, please wait $remaining seconds before using wall or echo again. Thanks!"
+            else
+                /usr/bin/wall "${input#wall }"
+                LAST_EXECUTED=$now
+            fi
+            ;;
+        *)
+            echo "Unknown command: $input"
+            $input >> "$LOG_FILE" 2>&1
+            ;;
+    esac
+done
