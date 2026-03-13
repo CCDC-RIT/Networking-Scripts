5d072a7392f794a104fc0e0ce4080d8f1c0d810f
Tri Tran <chemandtech874@gmail.com>
2026-03-12 23:21:09 -0400

    add inventory to palo start

--- palo-alto/start.sh
+++ palo-alto/start.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 start() {
     "$SCRIPT_DIR/core/backup.sh"
     "$SCRIPT_DIR/core/audit.sh"
+    "$SCRIPT_DIR/core/inventory.sh"
     "$SCRIPT_DIR/core/users.sh"
     "$SCRIPT_DIR/core/logging.sh"
     "$SCRIPT_DIR/core/secure.sh"
