87811efb975a984755eed807922e4d1f03a6275c
Tri Tran <chemandtech874@gmail.com>
2026-03-13 21:25:25 -0400

    correct conf path in palo common

--- palo-alto/core/common.sh
+++ palo-alto/core/common.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1091
-source "$SCRIPT_DIR/palo-alto.conf"
+source "$SCRIPT_DIR/../config.conf"
 
 mkdir -p "$BACKUP_DIR" "$LOG_DIR" "$CONFIG_DIR"
 
